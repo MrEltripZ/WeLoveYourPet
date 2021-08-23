286e4db76f287b35f863ecf2a400debb54785a7d
MrEltripZ <workingeltripz@gmail.com>
2021-08-23 01:12:58 +0100

    new project with home component html broken

---
+++ WLYP/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { GaugeModule } from 'angular-gauge';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HComponent } from './components/h/h.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    GaugeModule.forRoot(),
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatIconModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
